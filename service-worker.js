7ec33da5852be8f71577af3a73de5dfdbd90ab55
jaredjan <jaredjan.abayan@gmail.com>
2026-05-26 18:24:19 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -4,6 +4,8 @@ const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
+  "./service-worker.js",
+  "./icons/icon-180.png",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
 ];
@@ -30,8 +32,6 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
